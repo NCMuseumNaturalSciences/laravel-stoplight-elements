011e00fad11fdfbd23aa3f8b7bf6535b6eb3a656
Ben Norton <ben.norton@naturalsciences.org>
2021-12-10 14:08:24 -0500

    Update production-docs.blade.php

--- resources/views/production-docs.blade.php
+++ resources/views/production-docs.blade.php
@@ -1,1 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>{{ config('stoplight.title') }} | Production</title>
+    <!-- Embed elements Elements via Web Component -->
+    <script src="https://unpkg.com/@stoplight/elements@v7/web-components.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements@v7/styles.min.css">
+  </head>
+  <body>
 
+    <elements-api
+      apiDescriptionUrl="{{ config('stoplight.openapi.production.path') }}"
+      router="{{ config('stoplight.config.router') }}"
+      layout="{{ config('stoplight.config.layout') }}"
+    />
+
+  </body>
+</html>
