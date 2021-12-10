040384655f934fd3b8363f63ba52236ef5c8a39a
Ben Norton <ben.norton@naturalsciences.org>
2021-12-10 14:02:57 -0500

    Update stoplight.php

--- routes/stoplight.php
+++ routes/stoplight.php
@@ -3,8 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view(
-    config('stoplight.path.url'),
-    'stoplight::docs',
+    config('stoplight.development.path.url'),
+    'stoplight::development-docs',
 )->name(
-    config('stoplight.path.name')
+    config('stoplight.development.path.name')
+);
+
+Route::view(
+    config('stoplight.production.path.url'),
+    'stoplight::production-docs',
+)->name(
+    config('stoplight.production.path.name')
 );
